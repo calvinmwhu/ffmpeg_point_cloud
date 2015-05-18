3f31489e55b8360ec5c7aaf60cce86bcb11e65bd
Mingwei Hu <calvinmwhu@gmail.com>
2015-05-17 21:32:08 -0500

    god

--- client/client.cpp
+++ client/client.cpp
@@ -3,6 +3,7 @@
 #include <boost/thread.hpp>
 // #include <boost/asio.hpp>
 #include <boost/date_time.hpp>
+#include <vector>
 #include <iostream>
 #include <fstream>
 #include <stdio.h>
@@ -18,14 +19,18 @@
 
 
 const int MAXDATASIZE = 1024;
+typedef std::vector<uint8_t> buffer_type;
 
 
 typedef struct Buffer_t{
   bool busy;
   int id;
-  pthread_mutex_t* mutex;
-  pthread_cond_t* cond;
+  buffer_type data;
+  pthread_mutex_t mutex;
+  pthread_cond_t cond;
 }Buffer;
+
+Buffer buf[2];
 
 void play_video_func(){
   boost::posix_time::seconds workTime(1);
@@ -35,22 +40,124 @@ void play_video_func(){
   std::cout << "Play thread: finished" << std::endl;
 }
 
-void network_thread(){
-  // std::cout<<"reached here1"<<std::endl;
-  
-
+void flip(int &current){
+  if(current==0){
+    current=1;
+  }else{
+    current=0;
+  }
 }
 
-void* display_thread(void *ptr){
+void* network_thread(void *ptr){
+  int sockfd = *(int*)(ptr);
+  int next = 0;
+
+
   return NULL;
 }
 
-void connect_to_host(char *hostname, char *port){
-    
+void* display_thread(void *ptr){
+  int next = 0;
+
+  //there are 60 video clip to display (the client already knows this info)
+  for(int i=0; i<60; i++){
+
+  }
+
+  return NULL;
+}
+
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+  if (sa->sa_family == AF_INET) {
+    return &(((struct sockaddr_in*)sa)->sin_addr);
+  }
+
+  return &(((struct sockaddr_in6*)sa)->sin6_addr);
+}
+
+int connect_to_host(char *hostname, char *port){
+  int sockfd, numbytes;  
+  char buf[MAXDATASIZE];
+  struct addrinfo hints, *servinfo, *p;
+  int rv;
+  char s[INET6_ADDRSTRLEN];
+
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_UNSPEC;
+  hints.ai_socktype = SOCK_STREAM;
+
+  if ((rv = getaddrinfo(hostname, port, &hints, &servinfo)) != 0) {
+    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+    return -1;
+  }
+
+  // loop through all the results and connect to the first we can
+  for(p = servinfo; p != NULL; p = p->ai_next) {
+    if ((sockfd = socket(p->ai_family, p->ai_socktype,
+        p->ai_protocol)) == -1) {
+      perror("client: socket");
+      continue;
+    }
+    printf("connecting to %s at port %s\n", hostname, port);
+    if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1) {
+      close(sockfd);
+      perror("client: connect");
+      continue;
+    }
+    break;
+  }
+
+  if (p == NULL) {
+    fprintf(stderr, "client: failed to connect\n");
+    return -1;
+  }
+
+  inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr), s, sizeof s);
+  printf("client: connecting to %s\n", s);
+
+  freeaddrinfo(servinfo); // all done with this structure
+
+  return sockfd;
+
 }
 
 int main(int argc, char* argv[])
 {
-    network_thread();
-    return 0;
+  char host[100];
+  char port[100];
+  if(argc!=3){
+    sprintf(host, "%s", "10.0.0.9");
+    sprintf(port, "%s", "8000"); 
+  }else{
+    sprintf(host, "%s", argv[1]);
+    sprintf(port, "%s", argv[2]); 
+  }
+
+  //init buffer:
+  for(int i=0; i<2; i++){
+    buf[i].id = i;
+    buf[i].busy=false;
+    pthread_mutex_init(&buf[i].mutex, NULL);
+    pthread_cond_init(&buf[i].cond, NULL);
+  }
+
+  int sockfd = connect_to_host(host, port);
+  pthread_t network_t;
+  pthread_t display_t;
+
+  pthread_create(&network_t, NULL, network_thread, (void*)(&sockfd));
+  pthread_create(&display_t, NULL, display_thread, NULL);
+
+  pthread_join(network_t, NULL);
+  pthread_join(display_t, NULL);
+
+  for(int i=0; i<2; i++){
+    pthread_mutex_destroy(&buf[i].mutex);
+    pthread_cond_destroy(&buf[i].cond);
+  }
+
+  return 0;
 }
